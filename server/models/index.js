642ee7538f0c4846b81ce3961a3f4bbc3143c77e
KungFuTurtle <m.rfischer5235@gmail.com>
2023-07-20 13:14:39 -0500

    good vibes model and add good vibe made

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,3 @@
 const User = require('./User')
-
-module.exports = { User};
+const goodVibes = require('./goodVibes')
+module.exports = { User, goodVibes};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,37 +1,41 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Auth{
-    token:String
+type Auth {
+    token: String
     user: User
-   
 }
 
-type User{
-    _id:ID
-    username:String
-    email:String!
-    goodvibesList:String
-    password:String!
-
-      
+type User {
+    _id: ID
+    username: String
+    email: String!
+    password: String!
+    savedgoodVibes: [goodVibes]
 }
 
-
-
-
-type Mutation{
-    createUser(username:String!, email:String!, password:String!, ):Auth
-    loginAsUser(username: String!, password: String!,):Auth
-    updateUser(_id:ID!, username:String!, email:String!, goodvibesList:String):User
-    removeUser(_id:ID!):User
- 
+type goodVibes {
+    _id: ID
+    thegoodVibe: String
 }
 
+input goodVibesInput {
+    thegoodVibe: String
+}
 
-  type Query{
-    User(_id: ID): User
-    allUser: [User]!
+type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    loginAsUser(username: String!, password: String!): Auth
+    updateUser(_id: ID!, username: String!, email: String!, goodVibesList: String): User
+    removeUser(_id: ID!): User
+    addgoodVibes(savedgoodVibes: goodVibesInput): goodVibes
+}
+
+type Query {
+    user(_id: ID): User
+    allUsers: [User]!
+    allgoodVibes: [goodVibes]
+    goodVibe(_id: ID): goodVibes
 }
 `
-module.exports=typeDefs
+module.exports = typeDefs;
